cc525942a11727dcc53a19f0560187c2ad53f4f8
ling php <php@highqb.com>
2017-01-10 09:25:33 +0800

    flytcloud order submit

--- Flytcloud_Order_Submit.php
+++ Flytcloud_Order_Submit.php
@@ -6,7 +6,7 @@ $helper=Mage::helper('flytcloud');
 
 $collection = Mage::getResourceModel('flytcloud/order_shipping_status_collection')
     ->addFieldToFilter('shipping_status',array('eq'=>'3'))
-    ->setPageSize(5)
+    ->setPageSize(30)
     ->load();
 
 try{
@@ -20,5 +20,5 @@ try{
         }
     }
 }catch(Extractor $e){
-    
+
 }
