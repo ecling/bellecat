4c6c132677fbf8a30a44ee87c0f0cc6df70bea46
ling php <php@highqb.com>
2017-03-21 18:39:17 +0800

    fly submit bug

--- Flytcloud_Order_Submit.php
+++ Flytcloud_Order_Submit.php
@@ -2,6 +2,7 @@
 include('app/Mage.php');
 Mage::app();
 
+$adapter = Mage::getSingleton('core/resource')->getConnection('core_write');
 $helper=Mage::helper('flytcloud');
 
 $collection = Mage::getResourceModel('flytcloud/order_shipping_status_collection')
@@ -11,9 +12,18 @@ $collection = Mage::getResourceModel('flytcloud/order_shipping_status_collection
 
 try{
     foreach($collection as $_item){
+        if($_item->getFlytcloudOrderId()||$_item->getShippingStatus()==5){
+            continue;
+        }
+        $where = 'id='.$_item->getId();
+        $adapter->update('flytcloud_order_shipping_status',array('shipping_status'=>5),$where);
         $order = Mage::getModel('sales/order')->load($_item->getOrder());
         if($helper->isSameAddress($order)){
-            $helper->submitOrderToFlytcloud($order);
+            Mage::log($_item->getOrder(),null,'fly_submmit.log');
+            $submit = $helper->submitOrderToFlytcloud($order);
+            if(!$submit){
+                $adapter->update('flytcloud_order_shipping_status',array('shipping_status'=>3),$where);
+            }
         }else{
             $_item->setShippingStatus('4')
                 ->save();
