de706b94c9842092de7981af628b5f2529902c29
ling adam <997506412@qq.com>
2018-01-02 01:51:43 -0600

    201801021549

--- Facebook_Feed.php
+++ Facebook_Feed.php
@@ -6,7 +6,7 @@ $ob = Mage::getModel('Facebook_AdsToolbox/observer');
 $obins = new $ob;
 $use_cache = false;
 $store_id = 1;
-$currency = 'GBP';
+$currency = 'SEK';
 $obins->internalGenerateFacebookProductFeed(false, $use_cache,$store_id,$currency);
 
 echo 'success';
